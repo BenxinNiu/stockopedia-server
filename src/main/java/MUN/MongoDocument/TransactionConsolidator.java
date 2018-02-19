9d2f3f13f0d5dbb846ce0e157f575393483a1c86
bn2645@mun.ca <benxinniu880808>
2018-02-19 16:48:00 -0330

    implemented transcation fetching

--- src/main/java/MUN/MongoDocument/TransactionConsolidator.java
+++ src/main/java/MUN/MongoDocument/TransactionConsolidator.java
@@ -8,33 +8,76 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "Transcation")
 public class TransactionConsolidator {
 @Id
-private String id;
-private String userName;
-private String date;
-@Indexed(direction = IndexDirection.ASCENDING)
-private int pos;   //TODO add this in constructor??
-private Transcation transaction;
+private String _id;
+    private String trans_type;
+    private String book_value="NA";
+    private String ask_price;
+    private boolean fullfilled=false;
+    private String ticker;
+    private String trans_volume;
+    private String client_id;
+    private String trans_date;
+    private String fullfilled_date="NA";
 
-    public TransactionConsolidator(String id, String userName, String date, Transcation transaction) {
-        this.id = id;
-        this.userName = userName;
-        this.date = date;
-        this.transaction = transaction;
+    public TransactionConsolidator(String _id, String trans_type, String ask_price, String ticker, String trans_volume, String client_id, String trans_date) {
+        this._id = _id;
+        this.trans_type = trans_type;
+        this.ask_price = ask_price;
+        this.ticker = ticker;
+        this.trans_volume = trans_volume;
+        this.client_id = client_id;
+        this.trans_date = trans_date;
     }
 
-    public String getId() {
-        return id;
+    public void setBook_value(String book_value) {
+        this.book_value = book_value;
     }
 
-    public String getUserName() {
-        return userName;
+    public void setFullfilled(boolean fullfilled) {
+        this.fullfilled = fullfilled;
     }
 
-    public String getDate() {
-        return date;
+    public void setFullfilled_date(String fullfilled_date) {
+        this.fullfilled_date = fullfilled_date;
     }
 
-    public Transcation getTransaction() {
-        return transaction;
+    public String get_id() {
+        return _id;
+    }
+
+    public String getTrans_type() {
+        return trans_type;
+    }
+
+    public String getBook_value() {
+        return book_value;
+    }
+
+    public String getAsk_price() {
+        return ask_price;
+    }
+
+    public boolean isFullfilled() {
+        return fullfilled;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public String getTrans_volume() {
+        return trans_volume;
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public String getTrans_date() {
+        return trans_date;
+    }
+
+    public String getFullfilled_date() {
+        return fullfilled_date;
     }
 }
--- src/main/java/MUN/MongoRepo/TransactionCollectionRepo.java
+++ src/main/java/MUN/MongoRepo/TransactionCollectionRepo.java
@@ -1,8 +1,16 @@
 package MUN.MongoRepo;
 
 import MUN.MongoDocument.TransactionConsolidator;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
-public interface TransactionCollectionRepo extends MongoRepository<TransactionConsolidator,String> {
+import java.util.List;
 
+public interface TransactionCollectionRepo extends MongoRepository<TransactionConsolidator,String>,QueryDslPredicateExecutor<TransactionConsolidator> {
+   List<TransactionConsolidator> findBy_id(String _id);
+    @Query(value ="{trans_type:?0}")
+    List<TransactionConsolidator> findByTransType(String type);
+    @Query(value ="{fullfilled:?0}")
+    List<TransactionConsolidator> findByFullfill(String fullfilled);
 }
