3076060447c746b0b2cc40dea7628337fb8dd551
bn2645@mun.ca <benxinniu880808>
2018-02-21 13:59:52 -0330

    added option contracts query

---
+++ src/main/java/MUN/Controller/OptionController.java
@@ -0,0 +1,41 @@
+package MUN.Controller;
+
+import MUN.MongoDocument.DailyPriceConsolidator;
+import MUN.MongoDocument.OptionContractsConsolidator;
+import MUN.Service.FetchOption;
+import MUN.Service.FetchPriceData;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+@RestController
+public class OptionController {
+
+    @Autowired
+    FetchOption subscriber;
+
+    @RequestMapping("/options/{ticker}/{type}/strike/filter")
+    public List<OptionContractsConsolidator> queryOptions (@PathVariable("ticker") String ticker,
+                                                           @PathVariable("type") String type,
+                                                           @RequestParam(value ="price") double strike,
+                                                           @RequestParam(value = "condition") String condition){
+    List<OptionContractsConsolidator> result;
+
+    if(condition == "none"){
+        result = subscriber.queryByType(ticker, type);
+    }
+    else{
+        result= subscriber.queryByStrike(ticker,type,strike,condition);
+    }
+    return result;
+    }
+
+
+}
